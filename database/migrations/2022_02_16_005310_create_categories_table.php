94c829960f0bb7a5f2fa43c4e0dd01a517ea3a40
Norhan Taher <48496858+noorhann@users.noreply.github.com>
2022-02-17 11:31:17 +0200

    Update 2022_02_16_005310_create_categories_table.php

--- database/migrations/2022_02_16_005310_create_categories_table.php
+++ database/migrations/2022_02_16_005310_create_categories_table.php
@@ -11,8 +11,8 @@ class CreateCategoriesTable extends Migration
     {
         Schema::create('categories', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('slug');
+            $table->string('name_en');
+            $table->string('slug_en');
             $table->timestamps();
         });
     }
